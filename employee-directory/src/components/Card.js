396e229efcebbf87e5298a7372a8819d0d765fba
kevin <kevinhellman94@gmail.com>
2021-04-15 10:38:07 -0400

    Added fetch request

--- employee-directory/src/components/Card.js
+++ employee-directory/src/components/Card.js
@@ -1,15 +1,27 @@
+import React from 'react';
+
 class Card extends React.Component {
 
     state = {
-
+        people: []
     };
+
+    componentDidMount() {
+        fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then(people => {
+            this.setState({ people });
+        })
+    }
 
     render() {
         return (
             <div>
-                
+
             </div>
         );
     }
 
 }
+
+export default Card;
